f67cf4217fa4c4a0c6bb8d814e9d1e2618707c59
Isabirye Simon Peter <kaswadaniel100@gmail.com>
2025-03-15 23:18:16 +0300

    made more changes in the created sitemap

--- construction_site/urls.py
+++ construction_site/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),  
     path('services/', views.services, name='services'),
     path('contact/', views.contact, name='contact'),
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
 
